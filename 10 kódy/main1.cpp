04956ae50d58dc8c8dcb7d5591639b671f2abfff
GutenBerkk <167787299+GutenBerkk@users.noreply.github.com>
2025-08-14 01:37:03 +0200

    Update main1.cpp

--- 10 kódy/main1.cpp
+++ 10 kódy/main1.cpp
@@ -24,51 +24,51 @@ int main() {
         "Kradez vozidla",                       // 10-16
         "Podezrela osoba",                      // 10-17
         "Tresspasing",                          // 10-18
-        " ",                                     // 10-19 (volný)
+        " ",                                     // 10-19 (volnÃ½)
         "Lokace",                               // 10-20
-        " ",                                     // 10-21 (volný)
+        " ",                                     // 10-21 (volnÃ½)
         "Ignorujte prikaz",                     // 10-22
         "Dorazil na misto/scenu",               // 10-23
         "Opousti misto/scenu",                  // 10-24
         "Domaci nasili",                        // 10-25
-        " ",                                     // 10-26 (volný)
+        " ",                                     // 10-26 (volnÃ½)
         "Kontrola ridicskeho prukazu",         // 10-27
         "Kontrola registracni znacky",         // 10-28
         "Zkontrolujte, zda je osoba hledana",  // 10-29
         "Hledana osoba",                        // 10-30
         "Osoba neni v patrani, nema aktivni zatykac", // 10-31
         "Je potreba asistence",                 // 10-32
-        " ",                                     // 10-33 (volný)
-        " ",                                     // 10-34 (volný)
+        " ",                                     // 10-33 (volnÃ½)
+        " ",                                     // 10-34 (volnÃ½)
         "Vyklidte scenu",                       // 10-35
-        " ",                                     // 10-36 (volný)
-        " ",                                     // 10-37 (volný)
-        " ",                                     // 10-38 (volný)
-        " ",                                     // 10-39 (volný)
+        " ",                                     // 10-36 (volnÃ½)
+        " ",                                     // 10-37 (volnÃ½)
+        " ",                                     // 10-38 (volnÃ½)
+        " ",                                     // 10-39 (volnÃ½)
         " ",                                     // 10-40
         "Zahajeni patroly",                     // 10-41
         "Ukonceni patroly",                     // 10-42
         "Podejte informace",                    // 10-43
         "Osoba zemrela",                        // 10-44
-        " ",                                     // 10-45 (volný)
-        " ",                                     // 10-46 (volný)
-        " ",                                     // 10-47 (volný)
-        " ",                                     // 10-48 (volný)
-        " ",                                     // 10-49 (volný)
+        " ",                                     // 10-45 (volnÃ½)
+        " ",                                     // 10-46 (volnÃ½)
+        " ",                                     // 10-47 (volnÃ½)
+        " ",                                     // 10-48 (volnÃ½)
+        " ",                                     // 10-49 (volnÃ½)
         "Dopravni nehoda",                      // 10-50
         "Potrebuji odtahovku",                  // 10-51
         "Potrebuji zachranku/EMS",             // 10-52
         "Potrebuji hasice/FD",                  // 10-53
-        " ",                                     // 10-54 (volný)
+        " ",                                     // 10-54 (volnÃ½)
         "Ridic pod vlivem omamnych latek",     // 10-55
         "Chodec pod vlivem omamnych latek",    // 10-56
-        " ",                                     // 10-57 (volný)
-        " ",                                     // 10-58 (volný)
-        " ",                                     // 10-59 (volný)
+        " ",                                     // 10-57 (volnÃ½)
+        " ",                                     // 10-58 (volnÃ½)
+        " ",                                     // 10-59 (volnÃ½)
         "Ozbrojen strelnou zbrani",            // 10-60
         "Ozbrojen reznou zbrani",              // 10-61
         "Unos",                                 // 10-62
-        " ",                                     // 10-63 (volný)
+        " ",                                     // 10-63 (volnÃ½)
         "Sexualni napadeni",                    // 10-64
         "Preprava vezne do veznice",           // 10-65
         "Bezohledny ridic",                     // 10-66
@@ -77,31 +77,31 @@ int main() {
         "Vykradani domu",                        // 10-69
         "Pesi nahaneni - Suspect unika",       // 10-70
         "Dozor na scenu",                        // 10-71
-        " ",                                     // 10-72 (volný)
-        " ",                                     // 10-73 (volný)
-        " ",                                     // 10-74 (volný)
-        " ",                                     // 10-75 (volný)
-        " ",                                     // 10-76 (volný)
-        " ",                                     // 10-77 (volný)
-        " ",                                     // 10-78 (volný)
-        " ",                                     // 10-79 (volný)
+        " ",                                     // 10-72 (volnÃ½)
+        " ",                                     // 10-73 (volnÃ½)
+        " ",                                     // 10-74 (volnÃ½)
+        " ",                                     // 10-75 (volnÃ½)
+        " ",                                     // 10-76 (volnÃ½)
+        " ",                                     // 10-77 (volnÃ½)
+        " ",                                     // 10-78 (volnÃ½)
+        " ",                                     // 10-79 (volnÃ½)
         "Ujizdeni hlidce",                       // 10-80
-        " ",                                     // 10-81 (volný)
-        " ",                                     // 10-82 (volný)
-        " ",                                     // 10-83 (volný)
-        " ",                                     // 10-84 (volný)
-        " ",                                     // 10-85 (volný)
-        " ",                                     // 10-86 (volný)
-        " ",                                     // 10-87 (volný)
-        " ",                                     // 10-88 (volný)
-        " ",                                     // 10-89 (volný)
+        " ",                                     // 10-81 (volnÃ½)
+        " ",                                     // 10-82 (volnÃ½)
+        " ",                                     // 10-83 (volnÃ½)
+        " ",                                     // 10-84 (volnÃ½)
+        " ",                                     // 10-85 (volnÃ½)
+        " ",                                     // 10-86 (volnÃ½)
+        " ",                                     // 10-87 (volnÃ½)
+        " ",                                     // 10-88 (volnÃ½)
+        " ",                                     // 10-89 (volnÃ½)
         "Napomenuti policisty-zachranare",    // 10-90
-        " ",                                     // 10-91 (volný)
-        " ",                                     // 10-92 (volný)
-        " ",                                     // 10-93 (volný)
-        " ",                                     // 10-94 (volný)
+        " ",                                     // 10-91 (volnÃ½)
+        " ",                                     // 10-92 (volnÃ½)
+        " ",                                     // 10-93 (volnÃ½)
+        " ",                                     // 10-94 (volnÃ½)
         "Suspect in custody",                   // 10-95
-        " ",                                     // 10-96 (volný)
+        " ",                                     // 10-96 (volnÃ½)
         "Na ceste -",                           // 10-97
         "Pokracuji v hlidce",                   // 10-98
         "Officer v nouzi"                       // 10-99
@@ -149,6 +149,7 @@ int main() {
         else
         {
             std::cout << "Spatne, spravna odpoved je 10-" << randomNum << ", 10-" << num << " se ve meste nepouziva! " << std::endl;
+            bad++;
         }
     }
     accuracy = static_cast<double>(correct) / (correct + bad) * 100;
